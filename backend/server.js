d4757b2253760cff3f53f74cd8b0a0913e844ed6
ShimanshuChauhan <shivanshuchauhan699@gmail.com>
2025-06-23 19:55:27 +0530

    chore: created basic shell for backend

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+
+import app from './app.js';
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
