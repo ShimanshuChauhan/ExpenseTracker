37fefd8eed7867a3cd808de6975908cdf797a9eb
ShimanshuChauhan <shivanshuchauhan699@gmail.com>
2025-06-23 20:14:08 +0530

    fix : fixed global error handling in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 import 'dotenv/config';
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 import app from './app.js';
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
 });
